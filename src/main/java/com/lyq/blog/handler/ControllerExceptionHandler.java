35ad3045ffd6461874a0b9551e7b3b3dba33e213
Tbthr <1598622578@qq.com>
2020-01-31 14:17:34 +0800

    3.1 构建与配置/3.2 异常处理

---
+++ src/main/java/com/lyq/blog/handler/ControllerExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.lyq.blog.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    //    private final Logger logger = LoggerFactory.getLogger(ControllerExceptionHandler.class);
+
+    @ExceptionHandler({Exception.class})
+    public ModelAndView handleException(HttpServletRequest request, Exception e) throws Exception {
+
+        log.error("Request URL : {} , Exception : {}", request.getRequestURL(), e);
+
+        if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null) {
+            throw e;
+        }
+
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("url", request.getRequestURL());
+        mav.addObject("exception", e);
+        mav.setViewName("error/error");
+
+        return mav;
+    }
+
+}
---
+++ src/main/java/com/lyq/blog/web/IndexController.java
@@ -0,0 +1,24 @@
+/**
+ * Author:    lyq_power
+ * Date:      2019/9/21 19:18
+ */
+package com.lyq.blog.web;
+
+import com.lyq.blog.NotFoundExcepiton;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        int a = 2 / 0;
+        String blog = null;
+        if (blog == null) {
+            throw new NotFoundExcepiton("博客找不到");
+        }
+        return "index.html";
+    }
+
+}
