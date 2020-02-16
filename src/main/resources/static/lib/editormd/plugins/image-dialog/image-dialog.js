004b7ae2cabf2a2fe700dad0e1f62acd77875d49
Tbthr <1598622578@qq.com>
2020-02-16 22:21:31 +0800

    6.3 博客展示优化、主页布局优化

--- src/main/resources/static/lib/editormd/plugins/image-dialog/image-dialog.js
+++ src/main/resources/static/lib/editormd/plugins/image-dialog/image-dialog.js
@@ -90,7 +90,7 @@
                 dialog = this.createDialog({
                     title: imageLang.title,
                     width: (settings.imageUpload) ? 465 : 380,
-                    height: 254,
+                    height: 400,
                     name: dialogName,
                     content: dialogContent,
                     mask: settings.dialogShowMask,
